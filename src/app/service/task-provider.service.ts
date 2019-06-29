2cd8e19f4a9dc6f87309ad9293ff3f2c25935906
BRUNO ZAMBOTE RIBEIRO <171151797@acad.dvuadmin.net>
2019-06-28 21:57:20 -0300

    final

--- src/app/service/task-provider.service.ts
+++ src/app/service/task-provider.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,16 @@ export class TaskProviderService {
 
   public createTask(newTask: any): Observable<any>{
     return this.http.post("http://www.mocky.io/v2/5d1562250e00005700a112c3",newTask)
+  }
+  
+  public getTask(id: number): Observable<any> {
+    return from(Promise.resolve({
+      completed: true,
+      creationDate: "2019-06-28T23:25:22.278Z",
+      description: "1231",
+      priority: "1",
+      title: "312",
+      _id: 1231
+    }));
   }
 }
